837759e72db1384e5059eb1226906b0779921aca
Cauê Ruan Kotarski <caue1017@hotmail.com>
2018-10-26 20:46:12 -0300

    atualizado

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function validar_checkbox(){
+    var checar = document.getElementsByName("atividade");
+    var count = 0;
+
+    for(i=0;i<checar.length;i++){
+        if(checar[i].checked == true){
+            count++;
+            break;
+        }
+        if(count==0){
+            do
+        }
+    }
+}
